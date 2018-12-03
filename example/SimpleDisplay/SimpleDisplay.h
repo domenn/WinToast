3646723366bddfca509599cadbafdae5230f6def
Domen Mori <domen.mory@gmail.com>
2018-12-03 15:00:15 +0100

    Attempt to link with mingw compiled software

---
+++ example/SimpleDisplay/SimpleDisplay.h
@@ -0,0 +1,4 @@
+#pragma once
+extern "C" {    // another way
+    void c_display();
+};
---
+++ example/console-example/TestConsumeMingw/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// #include "C:/Users/Domen/Everything/WinToast/WinToast-master/example/console-example/SimpleDisplay.h"
+#include "../../SimpleDisplay/SimpleDisplay.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    c_display();
+    return 0;
+}
